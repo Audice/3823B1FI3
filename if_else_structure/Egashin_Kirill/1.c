9f3c87b8827db395e3589f9f951f9bb39c423211
north-fog <severfog5690jp@gmail.com>
2023-09-15 16:13:06 +0300

    First and Second task

--- if_else_structure/Egashin_Kirill/1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-
-int main() {
-    double a, b;
-    scanf("%lf %lf", &a, &b);
-    printf("Sum - %lf, difference - %lf, product - %lf, quotient - %lf", a + b, a - b, a * b, a / b);
-}
