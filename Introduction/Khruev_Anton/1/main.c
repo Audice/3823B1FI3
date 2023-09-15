8a0b92f428e10dd8017f746b40ddb091ddedf951
pt11241 <html23232@gmail.com>
2023-09-15 16:15:05 +0300

    -

---
+++ Introduction/Khruev_Anton/1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a = 30;
+    int b = 6;
+    int sum = a + b;
+    int razn = a - b;
+    int prod = a*b;
+    int chastnoe = a / b;
+    printf("%d -сумма %d - разность %d - произведение %d - частное",sum,razn,prod,chastnoe);
+    }
+
+
+
---
+++ Introduction/Khruev_Anton/2/main_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int a = 2;
+    int b = 13;
+    int sr_ar = a*a/2 + b*b/2;
+    printf("%d-Среднее арифмитическое квадратов",sr_ar);
+    }
