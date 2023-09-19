1211ebc8fea2fb4aa5b9e5662a22a11038c9c9da
Michail <vonamorm@mail.ru>
2023-09-19 21:47:53 +0300

    init

--- Introduction/Romanov_Michail/zadacha_6/z_6.c
+++ Introduction/Romanov_Michail/zadacha_6/z_6.c
@@ -3,13 +3,13 @@
 #include<stdlib.h>
 int main(){
     double x1, x2, x3, y1, y2, y3, s;
-    printf("Введите координаты x1,y1");
-    scanf("%lf %lf", x1, y1);
-     printf("Введите координаты x2,y2");
-    scanf("%lf %lf", x2, y2);
-     printf("Введите координаты x3,y3");
-    scanf("%lf %lf", x3, y3);
-    s = 0.5*fabs((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1));
+    printf("Введите координаты x1,y1 ");
+    scanf("%lf %lf", &x1,&y1);
+     printf("Введите координаты x2,y2 ");
+    scanf("%lf %lf", &x2, &y2);
+     printf("Введите координаты x3,y3 ");
+    scanf("%lf %lf", &x3, &y3);
+    s = 0.5*fabs((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1));//формула взята с сайта https://www.treugolniki.ru/ploshhad-treugolnika-po-koordinatam-vershin/
 printf("%lf", s);
    
 
