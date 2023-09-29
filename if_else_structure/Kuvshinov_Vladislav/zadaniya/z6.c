c139ae65acbf72610fa38996e78553aff63c3dbd
vlados1487 <vga.pan@mail.ru>
2023-09-29 09:29:16 +0300

    otpr

--- if_else_structure/Kuvshinov_Vladislav/zadaniya/z6.c
+++ if_else_structure/Kuvshinov_Vladislav/zadaniya/z6.c
@@ -4,13 +4,13 @@
 void main(){
     int a, b, c, d, k;
     scanf("%d %d %d %d", &a, &b, &c, &d);
-    if(a / b == 1 || a % b != 0){
+    if(a != b && a != c && a != d){
         printf("1");
-    }else if(b / c == 1 || b % c != 0){
+    }else if(b != c && b != a && b != d){
         printf("2");
-    }else if(c / a == 1 || c % a != 0){
+    }else if(c != a && c != b && c != d){
         printf("3");
-    }else if(d / b == 1 || d % b != 0){
+    }else if(d != a && d != b && d != c){
         printf("4");
     }
 
