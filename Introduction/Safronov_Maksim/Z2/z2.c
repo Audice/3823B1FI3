48859880797ea6c7df6bbc1e3cb59866a2b6a64e
SafronovMaks1m <144445164+SafronovMaks1m@users.noreply.github.com>
2024-02-26 00:36:16 +0300

    Update z2.c

--- Introduction/Safronov_Maksim/Z2/z2.c
+++ Introduction/Safronov_Maksim/Z2/z2.c
@@ -1,10 +1,52 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-void main(){
-    double a, b, c, d;
-    scanf("%lf %lf", &a, &b);
-    c = (pow(a, 2) + pow(b, 2)) / 2;
-    d = (double)(abs(a) + abs(b)) / 2;
-    printf("Среднее арифметическое квадратов %lf, Среднее арифметическое модулей %lf", c, d);
+#include "iostream"
+#include "sstream"
+#include "math.h"
+
+class Franction{
+    private:
+        double _number;
+        bool _znak = false;
+        std::string _str;
+    public:
+        Franction(double num){
+            if (num == 0.0){
+                throw "Error! num is 0!";}
+            if (num<0) {_znak = true;}
+            _number = abs(num); std::ostringstream strs; strs << _number; _str = strs.str();}
+        void reduction_franction(int* mas){
+            int a = mas[0], b = mas[1];
+            while (a!=b){
+                if (a > b) {a = a-b;}
+                else {b = b-a;}}
+            mas[0]/=a; mas[1]/=a;}
+        void real_is_franction(){
+            int full = std::stoi( _str ), real = 0, total = 0; bool flag = false;
+            for (int i = 0; i<_str.length(); i++){
+                if (flag == true){
+                    char s = _str[i]; 
+                    real=real*10+(int(s)-48);
+                    total+=1;}
+                if (_str[i] == '.'){
+                    flag = true;}}
+            full = full*pow(10, total)+real; 
+            real = pow(10, total);
+            int* mas = new int[2]; mas[0] = full, mas[1] = real;
+            std::cout << "Вещественное число "; if (_znak == true) std::cout << "-";
+            if (real == 1){
+                std::cout << _number << " преобразуется в дробное число : ";
+                if (_znak == true) std::cout << "-";
+                std::cout << mas[0]<< std::endl;}
+            else{
+                reduction_franction(mas);
+                std::cout << _number << " преобразуется в дробное число: ";
+                if (_znak == true) std::cout << "-";
+                std::cout << mas[0] << "/" << mas[1] << std::endl;}
+            delete[] mas;}
+};
+
+int main(){
+    Franction franc = Franction(3.12);
+    franc.real_is_franction();
+    Franction franc2 = Franction(2.1);
+    franc2.real_is_franction();
 }
